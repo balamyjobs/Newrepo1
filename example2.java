fe53ec7b1dd0dfa663bdf4d10518a7ba61139713
“balamyjobs” <balamyjobs@hotmail.com>
2022-09-28 11:32:16 +0000

    new program entered

--- example2.java
+++ example2.java
@@ -1,12 +1,14 @@
-import java.util.ArrayList;
-
-public class Main { 
-  public static void main(String[] args) { 
-    ArrayList<String> cars = new ArrayList<String>();
-    cars.add("Volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-    cars.add("Mazda");
-    System.out.println(cars);
-  } 
+class Vehicle {
+  protected String brand = "Ford";
+  public void honk() {
+    System.out.println("Tuut, tuut!");
+  }
 }
+
+class Car extends Vehicle {
+  private String modelName = "Mustang";
+  public static void main(String[] args) {
+    Car myFastCar = new Car();
+    myFastCar.honk();
+    System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+  }
